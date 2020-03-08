f4aa11234ba351762404f0ab6cb23cb5e7166777
shuangchen <guschenwpi@gmail.com>
2020-03-08 19:05:38 -0400

    More function in the home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React, {Component} from "react";
-import logo from './logo.svg';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from './components/MenuComponent';
-import './App.css';
 import { DISHES } from './shared/dishes';
+import './App.css';
 
 class App extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
@@ -15,13 +15,13 @@ class App extends Component {
 
   render() {
     return (
-      <div>
+      <div className="App">
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu dishes={this.state.dishes} />
+        <Menu dishes={this.state.dishes}/>
       </div>
     );
   }
